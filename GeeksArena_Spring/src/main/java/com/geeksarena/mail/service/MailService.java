1a5329f52d5d16eb52466a4c06a2f4c586e91270
pritomprasantpaul <pritomprasantpaul@gmail.com>
2021-07-03 21:06:36 +0530

    first commit

---
+++ GeeksArena_Spring/src/main/java/com/geeksarena/mail/service/MailService.java
@@ -0,0 +1,18 @@
+package com.geeksarena.mail.service;
+
+import java.util.List;
+
+import com.geeksarena.mail.bean.MailBean;
+import com.geeksarena.mail.bean.UserBean;
+
+
+public interface MailService {
+	public boolean userRegister(UserBean bean);
+	public UserBean login(String email, String password);
+	public List<MailBean> getAllMail(int from_id);
+	public UserBean searchByEmail(String email);
+	public boolean sendMail(String subject, String msg, int to_id, int from_id);
+	public boolean deleteMail(int mid);
+	public boolean draftMail(String subject, String msg, UserBean to, UserBean from);
+	public boolean changePassword(String email, String password);
+}
